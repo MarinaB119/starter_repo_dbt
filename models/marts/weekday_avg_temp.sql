f1129d533998e10ff859c52509dda185e6a12646
MarinaB119 <marina.bishay93@gmail.com>
2024-01-18 11:11:20 +0100

    code

--- models/marts/weekday_avg_temp.sql
+++ models/marts/weekday_avg_temp.sql
@@ -1,5 +1,5 @@
 WITH total_avg AS (
-    SELECT * 
+    SELECT 
     city, country, year, lat, lon, 
     avg(avgtemp_c) as avg_temp_month,
     max(maxtemp_c) as max_temp_month,
@@ -7,5 +7,4 @@ WITH total_avg AS (
     from {{ref("prep_temp")}}
     group by city, country, year, month, lat, lon
     )
-
 SELECT * from total_avg
