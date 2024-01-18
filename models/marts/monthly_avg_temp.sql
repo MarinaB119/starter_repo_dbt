89b98ced115c615495c55b4fc0d4b85d52a131b1
MarinaB119 <marina.bishay93@gmail.com>
2024-01-18 13:08:06 +0100

    push

--- models/marts/monthly_avg_temp.sql
+++ models/marts/monthly_avg_temp.sql
@@ -1,10 +1,10 @@
 WITH total_avg AS (
     SELECT 
-    city, country, year, lat, lon, 
+    city, country, lat, lon, 
     avg(avgtemp_c) as avg_temp_month,
     max(maxtemp_c) as max_temp_month,
     min(mintemp_c) as min_temp_month
     from {{ref("prep_temp")}}
-    group by city, country, year, month, lat, lon
+    group by city, country,  month, lat, lon
     )
 SELECT * from total_avg;
